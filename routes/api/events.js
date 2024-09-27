41cb7dc6146be4e81bacad9e268b6b3f09818ab5
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2024-09-27 19:38:34 +0300

    fix

--- routes/api/events.js
+++ routes/api/events.js
@@ -3,7 +3,6 @@ import eventsControllers from '../../controllers/events.js';
 
 const router = express.Router();
 
-
 router.get('', eventsControllers.getEvents);
 router.post('/:id/registration', eventsControllers.eventRegistration);
 router.get('/:id/participants', eventsControllers.getEventParticipants);
